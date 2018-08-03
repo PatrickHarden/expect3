2c7cf220b107777167cd1a11007243a31b4f7ab3
PatrickHarden <41203072+PatrickHarden@users.noreply.github.com>
2018-08-03 10:49:04 -0500

    Updated Nav

--- src/Nav.js
+++ src/Nav.js
@@ -36,7 +36,32 @@ import {
 
     return (
       <div className="navWrapper">
-
+        <Navbar color="white" dark fixed="top" expand="lg">
+            <Container>
+                <Row className="navRow">
+                    <Col xs="3">
+                        <NavbarBrand href="/"><img src={logo} /></NavbarBrand>                      
+                    </Col>
+                    <Col xs="9" className="text-right">
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <Link to="/" className="nav-link" onClick={this.toggle}>Home</Link>
+                            </NavItem>
+                            <MenuList toggle={this.toggle} />
+                            <NavItem>
+                                <Link to="/contact" className="nav-link" onClick={this.toggle}>Contact</Link>
+                            </NavItem>
+                            <NavItem>
+                                <a href="tel:9183799400" className="nav-link nav-phone" onClick={this.toggle}>(918) 379-9400</a>
+                            </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Col>
+                </Row>
+            </Container>
+        </Navbar>
       </div>
     );
   }
